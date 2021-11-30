7eccac1c9d1d6e54ba4dcfd1f85d7a6247479209
Lucas Iversen <43264353+LucasIversen@users.noreply.github.com>
2021-11-30 14:35:55 +0100

    Me do things

--- app/src/main/java/dk/au/mad21fall/projekt/rus_app/TutorView/TutorActivity.java
+++ app/src/main/java/dk/au/mad21fall/projekt/rus_app/TutorView/TutorActivity.java
@@ -7,10 +7,15 @@ import android.os.Bundle;
 import dk.au.mad21fall.projekt.rus_app.R;
 
 public class TutorActivity extends AppCompatActivity {
+    private String TAG = "TUTOR_VIEW";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor);
+
+
     }
+
+
 }
--- app/src/main/java/dk/au/mad21fall/projekt/rus_app/TutorView/TutorActivityViewModel.java
+++ app/src/main/java/dk/au/mad21fall/projekt/rus_app/TutorView/TutorActivityViewModel.java
@@ -1,7 +1,23 @@
 package dk.au.mad21fall.projekt.rus_app.TutorView;
 
+import android.app.Application;
+
+import java.util.ArrayList;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import dk.au.mad21fall.projekt.rus_app.Models.Tutor;
+import dk.au.mad21fall.projekt.rus_app.Repository;
 
 public class TutorActivityViewModel extends ViewModel {
+    MutableLiveData<ArrayList<Tutor>> tutors;
+    Repository repo;
+    String TAG = "TutorActivityViewModel";
+
+    public LiveData<ArrayList<Tutor>> getTutors() {
+
+        return tutors;
+    }
 
 }
