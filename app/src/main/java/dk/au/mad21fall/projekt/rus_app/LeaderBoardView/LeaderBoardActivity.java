3ddf0e6e9687e24320495fb0ee624593a697dcc1
HamannMojn <jacob@frost-hamann.dk>
2021-12-05 01:57:56 +0100

    fixed bug, hæhæ

--- app/src/main/java/dk/au/mad21fall/projekt/rus_app/LeaderBoardView/LeaderBoardActivity.java
+++ app/src/main/java/dk/au/mad21fall/projekt/rus_app/LeaderBoardView/LeaderBoardActivity.java
@@ -103,7 +103,7 @@ public class LeaderBoardActivity extends AppCompatActivity {
         decrement.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                team.incrementAmount();
+                team.decrementAmount();
                 amount.setText(Integer.toString(team.getAmount()));
             }
         });
